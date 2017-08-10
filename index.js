eb9645c52a3ff4284848f8b8a8a902e358bdc625
Richard Littauer <richard.littauer@gmail.com>
2017-08-10 11:42:56 -0400

    Extracting out libs, adding addPeriod function

--- index.js
+++ index.js
@@ -1,19 +1,4 @@
-function addStarsBadge (line) {
-  // Isolate []() links (We're only putting them in the square brackets)
-  // Check if there is already a GitHub stars badge in the []
-  // If the link is to a GitHub Repo in the (), add badge
-
-  var link = new RegExp('\[\(.*\)\](https:\/\/github.com\/\([^ )]*\))\(.*\)')
-
-  if (line.indexOf('https://github.com/') !== -1) {
-    if (line.indexOf('GitHub stars') === -1) {
-      console.log(line)
-      return line
-    }
-  }
-
-  return line
-}
+const lib = require('./lib/methods.js')
 
 function editLines (input) {
   // If Input is not an array, make it one
@@ -23,19 +8,10 @@ function editLines (input) {
   }
 
   return input.map((line) => {
-    // Add GitHub stars into Markdown link if it doesn't exist
-    // If line includes GitHub link
-    // TODO Make this regex more comprehensible using multiple JavaScript regexes
-    // sed  s/^\* \[\(.*\)\](https:\/\/github.com\/\([^ )]*\))\(.*\)/* [\1 \![GitHub stars]\(https:\/\/img.shields.io\/github\/stars\/\2.svg\)]\(https:\/\/github.com\/\2\)\3/' README.md
-    line = addStarsBadge(line)
+    line = lib.addStarsBadge(line)
+    line = lib.addPeriod(line)
     return line
   })
-
-  // Out of Scope of this module - rename module before publishing?
-  // TODO Create possible other module - list-cleaner?
-  // TODO Make sure that description matches `...) - desc`. format
-  // TODO Add periods on ends of descriptions
-  // TODO Automatically get descriptions (requires major refactor)
 }
 
-module.exports = {editLines, addStarsBadge}
+module.exports = editLines
--- test/test.js
+++ test/test.js
@@ -1,12 +1,13 @@
 /* eslint-env mocha */
 const assert = require('assert')
 const addStars = require('../.')
+const lib = require('../lib/methods.js')
 
 // TODO Remove this test and add real ones
 describe('Add Stars', function () {
   it('should be able to read a string', function () {
     var string = 'This is a string'
-    assert.equal(string, addStars.editLines(string))
+    assert.equal(string, addStars(string))
   })
 })
 
@@ -15,69 +16,83 @@ describe('Add Stars', function () {
 describe('Add stars badge', function () {
   it('should not replace strings without links', function () {
     var string = 'This is a string'
-    assert.equal(string, addStars.addStarsBadge(string))
+    assert.equal(string, lib.addStarsBadge(string))
   })
   
   it('should not edit strings with badges already', function () {
     var string = 'This is [good ![GitHub stars](https://img.shields.io/github/stars/good/good.svg)](https://github.com/good/good)'
-    assert.equal(string, addStars.addStarsBadge(string))
+    assert.equal(string, lib.addStarsBadge(string))
   })
   
   it('should not edit strings with badges already, anywhere in line', function () {
     var string = 'This is [good ![GitHub stars](https://img.shields.io/github/stars/good/good.svg)](https://github.com/good/good) with line after'
-    assert.equal(string, addStars.addStarsBadge(string))
+    assert.equal(string, lib.addStarsBadge(string))
   })
   
   it('should be able to parse links with parens inside of them', function () {
     var string = 'This is a [ test(test) ](https://github.com/test/test)'
     var newString = 'This is a [ test(test) ![GitHub stars](https://img.shields.io/github/stars/test/test.svg)](https://github.com/test/test)'
-    assert.equal(newString, addStars.addStarsBadge(string))
+    assert.equal(newString, lib.addStarsBadge(string))
   })
   
   it('should only touch links to github', function () {
     var string = 'This is a [string](http://example.com)'
-    assert.equal(string, addStars.addStarsBadge(string))
+    assert.equal(string, lib.addStarsBadge(string))
   })
   
   it('should only touch links to github even if there are other github links', function () {
     var string = 'This is a [string](http://example.com) [github](https://github.com/test/test)'
-    assert.equal(string, addStars.addStarsBadge(string))
+    assert.equal(string, lib.addStarsBadge(string))
   })
   
   it('should only touch links to github if they are to repos', function () {
     var string = 'This is a [string](http://example.com) [github](https://github.com)'
-    assert.equal(string, addStars.addStarsBadge(string))
+    assert.equal(string, lib.addStarsBadge(string))
     string = 'This is a [string](http://example.com) [github](https://github.com/user)'
-    assert.equal(string, addStars.addStarsBadge(string))
+    assert.equal(string, lib.addStarsBadge(string))
     string = 'This is a [string](http://example.com) [github](https://github.com/user/repo/issues)'
-    assert.equal(string, addStars.addStarsBadge(string))
+    assert.equal(string, lib.addStarsBadge(string))
 
     string = 'This is a [string](http://example.com) [github](https://github.com/user/repo)'
     var newString = 'This is a [string](http://example.com) [github ![GitHub stars](https://img.shields.io/github/stars/user/repo.svg)](https://github.com/user/repo)'
-    assert.equal(newString, addStars.addStarsBadge(string))
+    assert.equal(newString, lib.addStarsBadge(string))
   })
   
   it('should be able to change multiple links', function () {
     var string = 'This is [github](http://github.com/user/repo) [github](https://github.com/user/repo)'
     var newString = 'This is [github ![GitHub stars](https://img.shields.io/github/stars/user/repo.svg)](https://github.com/user/repo)) [github ![GitHub stars](https://img.shields.io/github/stars/user/repo.svg)](https://github.com/user/repo)'
-    assert.equal(newString, addStars.addStarsBadge(string))
+    assert.equal(newString, lib.addStarsBadge(string))
   })
   
   it('should update GitHub Stars badge repo if it doesn\'t match the link', function () {
     var string = 'This is a [test ![GitHub stars](https://img.shields.io/github/stars/test/test.svg)](https://github.com/test/CHANGE)'
     var newString = 'This is a [test ![GitHub stars](https://img.shields.io/github/stars/test/CHANGE.svg)](https://github.com/test/CHANGE)'
-    assert.equal(newString, addStars.addStarsBadge(string))
+    assert.equal(newString, lib.addStarsBadge(string))
   })
   
   it('should update GitHub Stars badge user if it doesn\'t match the link', function () {
     var string = 'This is a [test ![GitHub stars](https://img.shields.io/github/stars/test/test.svg)](https://github.com/CHANGE/test)'
     var newString = 'This is a [test ![GitHub stars](https://img.shields.io/github/stars/CHANGE/test.svg)](https://github.com/CHANGE/test)'
-    assert.equal(newString, addStars.addStarsBadge(string))
+    assert.equal(newString, lib.addStarsBadge(string))
   })
   
   it('should update GitHub Stars badge user and repo if it doesn\'t match the link', function () {
     var string = 'This is a [test ![GitHub stars](https://img.shields.io/github/stars/test/test.svg)](https://github.com/CHANGE/CHANGE)'
     var newString = 'This is a [test ![GitHub stars](https://img.shields.io/github/stars/CHANGE/CHANGE.svg)](https://github.com/CHANGE/CHANGE)'
-    assert.equal(newString, addStars.addStarsBadge(string))
+    assert.equal(newString, lib.addStarsBadge(string))
+  })
+})
+
+describe('addPeriod', function () {
+  it('should add a period at the end of the line', function (done) {
+    var string = 'This is a new line'
+    assert.equal(string + '.', lib.addPeriod(string))
+    done()
+  })
+
+  it('should not add a period if one exists', function (done) {
+    var string = 'This is a new line.'
+    assert.equal(string, lib.addPeriod(string))
+    done()
   })
 })
