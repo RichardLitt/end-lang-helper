ec6f3ebdc541e4263a6ef19cced2597186535237
Richard Littauer <richard.littauer@gmail.com>
2017-09-03 17:23:34 -0400

    Cleanup module

---
+++ README.md
@@ -0,0 +1,33 @@
+# end-lang-helper
+
+> A helper for RichardLitt/endangered-languages
+
+This is a helper tool for [RichardLitt/endangered-languages](https://github.com/RichardLitt/endangered-languages).
+
+## Install
+
+```
+npm i -g end-lang-helper
+```
+
+## Usage
+
+```
+end-lang-helper README.md
+```
+
+### Testing
+
+```
+npm test
+```
+
+## Contribute
+
+Feel free! Open an issue if there's functionality you'd like, or PR.
+
+My code is not great. If you have improvements, I'd love to see them.
+
+## License
+
+[MIT](LICENSE) © Richard Littauer 2017
--- cli.js
+++ cli.js
@@ -6,10 +6,10 @@ const addStars = require('./index.js')
 const Promise = require('bluebird')
 const cli = meow(`
   Usage
-    $ add-stars README.md
+    $ end-lang-helper README.md
 `)
 
-var fs = require('fs'),
+var fs = require('graceful-fs'),
     readline = require('readline');
 
 var rd = readline.createInterface({
@@ -19,15 +19,11 @@ var rd = readline.createInterface({
 });
 
 rd.on('line', function(line) {
-  console.log(addStars(line, {ignore: '<!-- ell:ignore -->'}))
+  addStars(line, {ignore: '<!-- ell:ignore -->'})
 });
 
+// TODO Add a better helper
 // if (cli.input.length === 0) {
 //   console.error('Input file required')
 //   process.exit(1)
 // }
-// 
-// Promise.resolve(addStars(cli.input[0], {ignore: '<!-- ell:ignore -->'}))
-// .then(data => {
-//   console.log(data)
-// })
